bbf7d21a06c007c9beb4dbbc5f51bf03cc342bdf
Tamas Gemes <tamas.gemes@gmail.com>
2021-02-11 10:43:26 +0100

    better naming

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export * from './any';
 export * from './array';
 export * from './boolean';
+export * from './check';
 export { nullOr } from './common';
 export * from './date';
 export * from './date-time';
-export * from './getters';
 export * from './number';
 export * from './object';
 export * from './record';
@@ -13,6 +13,5 @@ export * from './string-format';
 export * from './tuple';
 export * from './types';
 export * from './url';
-export * from './validation-error';
 
 // intentionally left empty
