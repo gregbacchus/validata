a47d743ab9d719402d7a24aeb2f257eb5e59bdb2
Tamas Gemes <tamas.gemes@directed.global>
2024-11-29 07:37:52 +0100

    chore: add async check

--- src/check.ts
+++ src/check.ts
@@ -1,4 +1,4 @@
-import { isIssue, Issue, Path, ValueProcessor } from './types';
+import { AsyncValueProcessor, isIssue, Issue, Path, Result, ValueProcessor } from './types';
 
 export class ValidationError extends Error {
   constructor(public readonly issues: Issue[]) {
@@ -10,10 +10,18 @@ export class ValidationError extends Error {
   }
 }
 
-export const check = <T>(check: ValueProcessor<T>, value: () => unknown, path: Path | Path[] = []): T => {
-  const result = check.process(value(), Array.isArray(path) ? path : [path]);
-  if (isIssue(result)) {
-    throw new ValidationError(result.issues);
-  }
+const getValue = <T>(result: Result<T>): T => {
+  if (isIssue(result)) throw new ValidationError(result.issues);
+
   return result.value;
+};
+
+export const check = <T>(valueProcessor: ValueProcessor<T>, value: () => unknown, path: Path | Path[] = []): T => {
+  const result = valueProcessor.process(value(), Array.isArray(path) ? path : [path]);
+  return getValue(result);
+};
+
+export const checkAsync = async <T>(asyncValueProcessor: AsyncValueProcessor<T>, value: () => unknown | Promise<unknown>, path: Path | Path[] = []): Promise<T> => {
+  const result = await asyncValueProcessor.process(await value(), Array.isArray(path) ? path : [path]);
+  return getValue(result);
 };
--- src/common.test.ts
+++ src/common.test.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon';
 import { asArray, isArray, maybeArray, maybeAsArray } from './array';
 import { asBoolean, isBoolean, maybeAsBoolean, maybeBoolean } from './boolean';
+import { checkAsync } from './check';
 import { asNullable, isNullable } from './common';
 import { asDate, isDate, maybeAsDate, maybeDate } from './date';
 import { asDateTime, isDateTime, maybeAsDateTime, maybeDateTime } from './date-time';
@@ -10,15 +11,15 @@ import { isRecord, maybeRecord } from './record';
 import { asString, isString, maybeAsString, maybeString } from './string';
 import { expectValue } from './test-helpers';
 import { isTuple, maybeTuple } from './tuple';
-import { ValueProcessor } from './types';
+import { AsyncValueProcessor, ValueProcessor } from './types';
 import { asUrl, isUrl, maybeAsUrl, maybeUrl } from './url';
 
-describe('createNullableCheck', () => {
+describe('common tests', () => {
   const dateTime = DateTime.local();
   const date = new Date();
   const url = new URL('http://localhost');
 
-  it.each([
+  const testCases = [
     [isArray, ['a', 'b']],
     [maybeArray, undefined],
     [asArray, 'a'],
@@ -64,99 +65,117 @@ describe('createNullableCheck', () => {
     [maybeUrl, undefined],
     [asUrl, url.toString()],
     [maybeAsUrl, undefined],
-  ])('createIsNullableCheck %#', (
-    check: (...args: any[]) => ValueProcessor<unknown>,
-    value: unknown,
-    contract: unknown = undefined
-  ) => {
-    const fut = check(contract);
-    const futNull = isNullable(check(contract));
-    expectValue(futNull, null, null);
-    expect(fut.process(value)).toEqual(futNull.process(value));
+  ] as const;
+
+  describe('createNullableCheck', () => {
+    it.each(testCases)('createIsNullableCheck %#', (
+      check: (...args: any[]) => ValueProcessor<unknown>,
+      value: unknown,
+      contract: unknown = undefined
+    ) => {
+      const fut = check(contract);
+      const futNull = isNullable(check(contract));
+      expectValue(futNull, null, null);
+      expect(fut.process(value)).toEqual(futNull.process(value));
+    });
+
+    it.each([
+      [maybeArray],
+      [maybeArray, undefined, [1, 2, 3]],
+      [maybeArray, [1, 2, 3], null, [1, 2, 3]],
+
+      [maybeAsArray],
+      [maybeAsArray, undefined, [1, 2, 3]],
+      [maybeAsArray, [1, 2, 3], null, [1, 2, 3]],
+
+      [maybeBoolean],
+      [maybeBoolean, undefined, true],
+      [maybeBoolean, true, null, true],
+      [maybeAsBoolean],
+      [maybeAsBoolean, undefined, true],
+      [maybeAsBoolean, 'true', null, true],
+
+      [maybeDateTime],
+      [maybeDateTime, undefined, dateTime],
+      [maybeDateTime, dateTime, null, dateTime],
+      [maybeAsDateTime],
+      [maybeAsDateTime, undefined, dateTime],
+      [maybeAsDateTime, dateTime, null, dateTime],
+
+      [maybeDate],
+      [maybeDate, undefined, date],
+      [maybeDate, date, null, date],
+      [maybeAsDate],
+      [maybeAsDate, undefined, date],
+      [maybeAsDate, date.toISOString(), null, date],
+
+      [maybeNumber],
+      [maybeNumber, undefined, 5],
+      [maybeNumber, 5, null, 5],
+      [maybeAsNumber],
+      [maybeAsNumber, undefined, 5],
+      [maybeAsNumber, undefined, () => 5, 5],
+      [maybeAsNumber, '5', null, 5],
+
+      [maybeObject],
+      [maybeObject, undefined, { key: 'value' }],
+      [maybeObject, { key: 'value' }, null, { key: 'value' }],
+      [maybeAsObject],
+      [maybeAsObject, undefined, { key: 'value' }],
+      [maybeAsObject, { key: 'value' }, null, { key: 'value' }],
+
+      [maybeRecord],
+      [maybeRecord, undefined, { key: 'value' }],
+      [maybeRecord, { key: 'value' }, null, { key: 'value' }],
+
+      [maybeString],
+      [maybeString, undefined, 'string'],
+      [maybeString, 'string', null, 'string'],
+      [maybeAsString],
+      [maybeAsString, undefined, 'string'],
+      [maybeAsString, 'string', null, 'string'],
+
+      [maybeTuple],
+      [maybeTuple, undefined, [1, 'text'], [1, 'text'], [isNumber(), isString()]],
+      [maybeTuple, [1, 'text'], null, [1, 'text'], [isNumber(), isString()]],
+
+      [maybeUrl],
+      [maybeUrl, undefined, url],
+      [maybeUrl, url, null, url],
+      [maybeAsUrl],
+      [maybeAsUrl, undefined, url],
+      [maybeAsUrl, url.toString(), null, url],
+    ])('createAsNullableCheck %#', (
+      check: (...args: any[]) => ValueProcessor<unknown>,
+      value: unknown = undefined,
+      defaultValue: unknown = null,
+      expectedValue: unknown = defaultValue,
+      contract: unknown = undefined) => {
+      const futNull = asNullable(check(contract), { default: defaultValue });
+      expect(futNull.process(value)).toEqual({ value: expectedValue });
+    });
+
+    it('result is intact during an issue detection', () => {
+      const fut = isObject({ key: isBoolean() });
+      const futNull = isObject({ key: asNullable(isBoolean()) });
+      const result = fut.process({ key: [] });
+      const resultNull = futNull.process({ key: [] });
+
+      expect(result).toEqual(resultNull);
+    });
   });
 
-  it.each([
-    [maybeArray],
-    [maybeArray, undefined, [1, 2, 3]],
-    [maybeArray, [1, 2, 3], null, [1, 2, 3]],
-
-    [maybeAsArray],
-    [maybeAsArray, undefined, [1, 2, 3]],
-    [maybeAsArray, [1, 2, 3], null, [1, 2, 3]],
-
-    [maybeBoolean],
-    [maybeBoolean, undefined, true],
-    [maybeBoolean, true, null, true],
-    [maybeAsBoolean],
-    [maybeAsBoolean, undefined, true],
-    [maybeAsBoolean, 'true', null, true],
-
-    [maybeDateTime],
-    [maybeDateTime, undefined, dateTime],
-    [maybeDateTime, dateTime, null, dateTime],
-    [maybeAsDateTime],
-    [maybeAsDateTime, undefined, dateTime],
-    [maybeAsDateTime, dateTime, null, dateTime],
-
-    [maybeDate],
-    [maybeDate, undefined, date],
-    [maybeDate, date, null, date],
-    [maybeAsDate],
-    [maybeAsDate, undefined, date],
-    [maybeAsDate, date.toISOString(), null, date],
-
-    [maybeNumber],
-    [maybeNumber, undefined, 5],
-    [maybeNumber, 5, null, 5],
-    [maybeAsNumber],
-    [maybeAsNumber, undefined, 5],
-    [maybeAsNumber, undefined, () => 5, 5],
-    [maybeAsNumber, '5', null, 5],
-
-    [maybeObject],
-    [maybeObject, undefined, { key: 'value' }],
-    [maybeObject, { key: 'value' }, null, { key: 'value' }],
-    [maybeAsObject],
-    [maybeAsObject, undefined, { key: 'value' }],
-    [maybeAsObject, { key: 'value' }, null, { key: 'value' }],
-
-    [maybeRecord],
-    [maybeRecord, undefined, { key: 'value' }],
-    [maybeRecord, { key: 'value' }, null, { key: 'value' }],
-
-    [maybeString],
-    [maybeString, undefined, 'string'],
-    [maybeString, 'string', null, 'string'],
-    [maybeAsString],
-    [maybeAsString, undefined, 'string'],
-    [maybeAsString, 'string', null, 'string'],
-
-    [maybeTuple],
-    [maybeTuple, undefined, [1, 'text'], [1, 'text'], [isNumber(), isString()]],
-    [maybeTuple, [1, 'text'], null, [1, 'text'], [isNumber(), isString()]],
-
-    [maybeUrl],
-    [maybeUrl, undefined, url],
-    [maybeUrl, url, null, url],
-    [maybeAsUrl],
-    [maybeAsUrl, undefined, url],
-    [maybeAsUrl, url.toString(), null, url],
-  ])('createAsNullableCheck %#', (
-    check: (...args: any[]) => ValueProcessor<unknown>,
-    value: unknown = undefined,
-    defaultValue: unknown = null,
-    expectedValue: unknown = defaultValue,
-    contract: unknown = undefined) => {
-    const futNull = asNullable(check(contract), { default: defaultValue });
-    expect(futNull.process(value)).toEqual({ value: expectedValue });
-  });
-
-  it('result is intact during an issue detection', () => {
-    const fut = isObject({ key: isBoolean() });
-    const futNull = isObject({ key: asNullable(isBoolean()) });
-    const result = fut.process({ key: [] });
-    const resultNull = futNull.process({ key: [] });
-
-    expect(result).toEqual(resultNull);
+  describe('checkAsync', () => {
+    it.each(testCases)('async check run %#', async (
+      check: (...args: any[]) => ValueProcessor<unknown>,
+      value: unknown,
+      contract: unknown = undefined
+    ) => {
+      const futAsync: AsyncValueProcessor<unknown> = {
+        process: (value) => Promise.resolve(check(contract).process(value)),
+      };
+      const result = checkAsync(futAsync, () => Promise.resolve(value));
+      await expect(result).resolves.not.toThrow();
+    });
   });
 });
